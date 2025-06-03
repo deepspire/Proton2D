959ffe9c02a005fb80d07ffbb8463943bb80f737
Monsler <105060825+Monsler@users.noreply.github.com>
2025-06-03 16:23:29 +0300

    Delete src/proton/logman.cc

--- src/proton/logman.cc
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "logman.hh"
-
-void ProtonLog(const char *msg)
-{
-    // TODO: добавить поддержку форматирования (%s, ...)
-    std::cout << "[BURGER2D] " << msg << std::endl;
-}
