10b0cac8c00eb6165c70bc03c845a68278454fe4
Monsler <105060825+Monsler@users.noreply.github.com>
2025-06-11 14:07:03 +0300

    progress bar update

--- src/proton/shapes/progressbar.hh
+++ src/proton/shapes/progressbar.hh
@@ -37,13 +37,18 @@ namespace Proton
                 Proton::Log("Progress value can't be lower than 0 and greater than 100! You're trying to set value: ", progress);
             }
         }
+        
+        int getProgress()
+        {
+            return this->progress;
+        }
 
         void paint(SDL_Renderer* render, int pX, int pY)
         {
             float progressWidth = (static_cast<float>(progress)/100.0f)*this->width-4;
             SDL_FRect bgRect = {static_cast<float>(this->x+pX), static_cast<float>(this->y+pY), static_cast<float>(this->width), static_cast<float>(this->height)};
             SDL_FRect progressRect = {static_cast<float>(this->x+pX+2), static_cast<float>(this->y+pY+2), static_cast<float>(progressWidth), static_cast<float>(this->height-4)};
-            SDL_SetRenderDrawColor(render, progressBarColor.getR()-50, progressBarColor.getG()-50, progressBarColor.getB()-50, progressBarColor.getA());
+            SDL_SetRenderDrawColor(render, progressBarColor.getR() > 0 ? progressBarColor.getR()-50 : 0, progressBarColor.getG() > 0 ? progressBarColor.getG()-50 : 0, progressBarColor.getB() > 0 ? progressBarColor.getB()-50 : 0, progressBarColor.getA());
             SDL_RenderFillRect(render, &bgRect);
             SDL_SetRenderDrawColor(render, progressBarColor.getR(), progressBarColor.getG(), progressBarColor.getB(), progressBarColor.getA());
             SDL_RenderFillRect(render, &progressRect);
